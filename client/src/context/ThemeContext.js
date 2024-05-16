20c77a77c71d4f53b2cfc61c08ae3ff5e541fca9
alex_hcj <alexhcj@yandex.ru>
2024-05-16 13:20:32 +0300

    fix: theme change rules

--- client/src/context/ThemeContext.js
+++ client/src/context/ThemeContext.js
@@ -1,6 +1,6 @@
-import {createContext, useContext, useEffect} from 'react'
-import {useLocalStorage} from "../hooks/useLocalStorage.hook";
-import {useThemeDetector} from "../hooks/useThemeDetector";
+import {createContext, useContext} from 'react'
+import {useLocalStorage} from '../hooks/useLocalStorage.hook'
+import {useThemeDetector} from '../hooks/useThemeDetector'
 
 const ThemeContext = createContext({})
 
@@ -10,10 +10,6 @@ export const useThemeContext = () => useContext(ThemeContext)
 export const ThemeProvider = ({children}) => {
     const isDarkTheme = useThemeDetector()
     const [theme, setTheme] = useLocalStorage('theme', isDarkTheme ? 'dark' : 'light')
-
-    useEffect(() => {
-        isDarkTheme ? setTheme('dark') : setTheme('light')
-    }, [isDarkTheme]);
 
     return <ThemeContext.Provider value={{theme, setTheme}}>{children}</ThemeContext.Provider>
 }

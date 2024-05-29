92e48981307b1fd2ff54a5e82bde26010b9121f4
alex_hcj <alexhcj@yandex.ru>
2024-05-29 16:33:40 +0300

    add: clear object from undefined

--- client/src/utils/string.js
+++ client/src/utils/string.js
@@ -2,3 +2,5 @@ export const slugToString = (slug) => slug.split('-').join(' ')
 export const capitalizeFirstChar = (str) => str[0].toUpperCase() + str.slice(1)
 export const stringDashedToNormal = (str) => str.split('-').join(' ')
 export const stringDashedToUnderscore = (str) => str.split('-').join('_')
+export const clearObject = (obj) =>
+  Object.fromEntries(Object.entries(obj).filter(([_, value]) => !!value))

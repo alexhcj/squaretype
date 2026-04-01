b179306bf1641d3f379194c20dd555dc190ca205
nanashi <generalovderzkij@gmail.com>
2026-04-01 15:19:24 +0300

    fix: search navigate to post

--- client/src/components/Search/SearchItem/SearchItem.jsx
+++ client/src/components/Search/SearchItem/SearchItem.jsx
@@ -6,7 +6,7 @@ import s from './SearchItem.module.sass'
 
 export const SearchItem = ({ title, slug, img, date, onClick }) => {
   return (
-    <NavLink className={s.item} to={`${slug}`} onClick={onClick}>
+    <NavLink className={s.item} to={`/${slug}`} onClick={onClick}>
       <img className={s.img} src={getSizedImg(img, '80x80')} alt={title} />
       <h3 className={s.title}>{title}</h3>
       <span className={s.date}>{transformPostDate(date)}</span>

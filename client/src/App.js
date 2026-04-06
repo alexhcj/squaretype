41ee43b4e9d9c76022793f17f027d1b21ab84055
Nanashi <generalovderzkij@gmail.com>
2026-04-06 16:02:03 +0300

    hotfix: duplicated SidebarModalProvider usage

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,12 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { AppRoutes } from './routes/routes'
 import { AuthProvider } from './context/AuthContext'
-import { SidebarModalProvider } from './context/SidebarModalContext'
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <SidebarModalProvider>
-          <AppRoutes />
-        </SidebarModalProvider>
+        <AppRoutes />
       </AuthProvider>
     </BrowserRouter>
   )
